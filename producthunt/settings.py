d9d692f974ca59bdf3d247a709483e4b30b82f80
Vishnu Kant Shukla <21shukla08@gmail.com>
2024-12-03 20:50:32 +0100

    Update settings.py
    small change

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6hyv52^o50t8$36^#*u-pae&3it2ek=tg5owq9nwvxx_w@20b2'
+SECRET_KEY = '#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
